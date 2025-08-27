48765b0e5cca68890bcdf9f288bfbaab354325f8
Alan Roger M. A. <91630631+AllenRodger22@users.noreply.github.com>
2025-08-27 11:01:00 -0300

    Update server.js
    teste

--- src/server.js
+++ src/server.js
@@ -1,16 +1,6 @@
-// src/server.js
-require('dotenv').config();              // 1) carrega env primeiro
-
-const app = require('./app');            // 2) importa app
-const { connectToDatabase } = require('./config/database'); // 3) DB (Mongo)
-
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-const startServer = async () => {
-  await connectToDatabase();             // conecta no Mongo
-  app.listen(PORT, () => {
-    console.log(`✅ Servidor rodando na porta ${PORT}`);
-  });
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
